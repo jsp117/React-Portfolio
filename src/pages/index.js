3289b9822f230393445f264336f3737f9f91e791
jsp <jonsp14@gmail.com>
2020-11-16 00:53:34 -0800

    github api possible fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,33 +12,43 @@ function Main() {
     useEffect(() => {
         var token = "toke91c0d6a2bcb84fd0f2db445a94f7eb4b12f35dfb"
         token = token.replace('toke', '');
-        fetch("https://api.github.com/users/jsp117/repos", {
-            method: "get",
-            headers: new Headers({
+        var obj = {
+            method: "GET",
+            headers: {
                 Accept: "application/vnd.github.v3+json",
                 Username: "jsp117",
-                Authorization: "token:" + token,
-            })
-        }).then(function (response) {
+                Authorization: "token: " + token,
+            }
+        }
+        fetch("https://cors-anywhere.herokuapp.com/https://api.github.com/users/jsp117/repos", obj
+            // method: "GET",
+            // headers: new Headers({
+            //     Accept: "application/vnd.github.v3+json",
+            //     Username: "jsp117",
+            //     Authorization: "token: " + token,
+            // })
+        ).then(function (response) {
             console.log(response);
         }).catch(function (error) {
             console.log("error", error);
         })
-        // var repos = axios({
-        //     url: "https://api.github.com/users/jsp117/repos",
-        //     method: "GET",
-        //     headers: {
-        //     Accept: "application/vnd.github.v3+json",
-        //     Username: "jsp117",
-        //     Authorization: "token: token",
-        // },
-        // }).then(function (response) {
-        //     return response.data;
-        // }).catch(function (error) {
-        //     console.log(error);
-        // });
-
-        // console.log("repos in main", repos)
+        // var repo = test();
+        // function test() {
+        //     axios({
+        //         url: "https://cors-anywhere.herokuapp.com/https://api.github.com/users/jsp117/repos",
+        //         method: "GET",
+        //         headers: {
+        //             Accept: "application/vnd.github.v3+json",
+        //             Username: "jsp117",
+        //             Authorization: "token:" + token,
+        //         },
+        //     }).then(function (response) {
+        //         return response.data;
+        //     }).catch(function (error) {
+        //         console.log(error);
+        //     });
+        // }
+        // console.log("repos in main", repo);
         // const response = API();
         // console.log("response on main", response);
     }, []);
@@ -46,16 +56,16 @@ function Main() {
     useEffect(() => {
     }, [page]);
 
-    function handlePage(event){
+    function handlePage(event) {
         event.preventDefault();
         let value = event.target.name;
-        if(value==="index"){
+        if (value === "index") {
             setPage("index");
-        }else if(value==="portfolio"){
+        } else if (value === "portfolio") {
             setPage("portfolio");
-        }else if(value==="contact"){
+        } else if (value === "contact") {
             setPage("contact");
-        }else if(value==="projects"){
+        } else if (value === "projects") {
             setPage("projects");
         }
     }
@@ -65,7 +75,7 @@ function Main() {
 
     return (
         <div className="container">
-            <Header page={page} click={handlePage}/>
+            <Header page={page} click={handlePage} />
             <section id="spacing"></section>
             <Card page={page} />
             <section id="spacing"></section>
