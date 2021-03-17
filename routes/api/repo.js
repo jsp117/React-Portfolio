98f573cc0c06520e3d4c055cc57b419556d1b872
jsp <jonsp14@gmail.com>
2021-03-16 17:21:45 -0700

    use config var

--- routes/api/repo.js
+++ routes/api/repo.js
@@ -5,8 +5,8 @@ const axios = require("axios");
 
 // get all repos
 router.route("/").get(async function (req, res) {
-    var token = "toke91c0d6a2bcb84fd0f2db445a94f7eb4b12f35dfb"
-    token = token.replace('toke', '');
+    var token = process.env.TOKEN;
+    // token = token.replace('toke', '');
     // get all repos from github
     let repos = await axios({
         url: "https://api.github.com/users/jsp117/repos",
